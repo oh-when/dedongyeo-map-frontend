7c0f49777335371765229368932e936b3fd2d68a
angrycatjenny <angrycatjenny@gmail.com>
2021-07-16 17:41:53 +0900

    popup type별 모달 띄우기

--- components/Popup/Popup.tsx
+++ components/Popup/Popup.tsx
@@ -5,6 +5,10 @@ import CourseShare, { Props as CourseShareProps } from './CourseShare';
 import { PopupType } from '~/@types/popup.d';
 import { popupState } from '~/lib/apollo/vars/global';
 import Modal from '~/components/_common/Modal';
+import SignIn from '~/components/User/SignIn/SignIn';
+import SignUp from '~/components/User/SignUp/SignUp';
+import ForgotId from '~/components/User/ForgotId/ForgotId';
+import ForgotPassword from '../User/ForgotPassword/ForgotPassword';
 
 /**
  * @description
@@ -36,6 +40,41 @@ const Popup: React.FC = () => {
     return (
       <Modal>
         <CourseShare zIndex="10001" {...props} />
+      </Modal>
+    );
+  }
+
+  if (popupType === PopupType.SIGN_IN) {
+    return (
+      <Modal>
+        <SignIn zIndex="10002" />
+      </Modal>
+    );
+  }
+
+  if (popupType === PopupType.SIGN_UP) {
+    console.log('SIGN_UP');
+    return (
+      <Modal>
+        <SignUp zIndex="10003" />
+      </Modal>
+    );
+  }
+
+  if (popupType === PopupType.FORGOT_ID) {
+    console.log('FORGOT_ID');
+    return (
+      <Modal>
+        <ForgotId zIndex="10004" />
+      </Modal>
+    );
+  }
+
+  if (popupType === PopupType.FORGOT_PASSWORD) {
+    console.log('FORGOT_PASSWORD');
+    return (
+      <Modal>
+        <ForgotPassword zIndex="10005" />
       </Modal>
     );
   }
