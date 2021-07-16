03b58dfa0bb2411f40ec0ebaa970bc4220e5aa90
angrycatjenny <angrycatjenny@gmail.com>
2021-07-16 17:54:51 +0900

    회원가입 컴포넌트 ( 서비스 이용약관 전체 회의 필요 )

---
+++ components/User/SignUp/SignUp.tsx
@@ -0,0 +1,281 @@
+import React, { useState } from 'react';
+import * as $ from './SignUpView';
+// import { useRouter } from 'next/router';
+import type { PopupChildProps } from '~/@types/popup.d';
+import { usePopupCloser } from '~/lib/apollo/hooks/usePopup';
+
+export type Props = PopupChildProps;
+const CHECKBOX_ID = 'daedong_chbox_allow';
+
+const SignUp: React.FC<Props> = ({ zIndex }) => {
+  // const { pathname } = useRouter();
+  const closePopup = usePopupCloser();
+
+  const [isAllAgree, setIsAllAgree] = useState(false);
+  const [agreeOne, setAgreeOne] = useState(true);
+  const [agreeTwo, setAgreeTwo] = useState(true);
+  const [agreeThree, setAgreeThree] = useState(true);
+  const [agreeFour, setAgreeFour] = useState(true);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [nickName, setNickName] = useState('');
+  const [phoneNum, setPhoneNum] = useState('');
+
+  const [isVaildEmail, setIsVaildEmail] = useState(true);
+  const [isValidPassword, setIsValidPassword] = useState(true);
+  const [isValidPasswordConfirm, setIsValidPasswordConfirm] = useState(true);
+  const [modalStep, setModalStep] = useState(1);
+
+  const emailRegExp = /^[0-9a-z]([-_\.]?[0-9a-z])*@[0-9a-z]([-_\.]?[0-9a-z])*\.[a-z]/;
+  const passwordRegExp = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/;
+  const phoneRegExp =
+    (/(^02.{0}|^01.{1}|[0-9]{3})([0-9]+)([0-9]{4})/, '$1-$2-$3');
+
+  const handleClickCloseButton = (e: React.MouseEvent) => {
+    e.preventDefault();
+    closePopup();
+  };
+
+  const handleEmail = (e: any) => {
+    const val = e.target.value;
+    if (emailRegExp.test(val)) {
+      // 사용중인 이메일 계정인지 쿼리 날려서 검증 필요
+      // setIsVaildEmail 결정
+      setEmail(e.target.value);
+    }
+  };
+
+  const handlePassword = (e: any) => {
+    const val = e.target.value;
+    setPassword(val);
+    if (passwordRegExp.test(val)) {
+      setIsValidPassword(true);
+    } else {
+      setIsValidPassword(false);
+    }
+  };
+
+  const handlePasswordConfirm = (e: any) => {
+    const val = e.target.value;
+    setPasswordConfirm(val);
+    if (isValidPassword) {
+      if (password === val) {
+        setIsValidPasswordConfirm(true);
+      } else {
+        setIsValidPasswordConfirm(false);
+      }
+    }
+  };
+
+  const handleNickname = (e: any) => {
+    const val = e.target.value;
+    setNickName(val);
+  };
+
+  const handlePhoneNum = (e: any) => {
+    setPhoneNum(autoHypenPhone(e.target.value));
+  };
+
+  const autoHypenPhone = (num: string) => {
+    num = num.replace(/[^0-9]/g, '');
+    let tmp = '';
+    if (num.length < 4) {
+      return num;
+    } else if (num.length < 7) {
+      tmp += num.substr(0, 3);
+      tmp += '-';
+      tmp += num.substr(3);
+      return tmp;
+    } else if (num.length < 11) {
+      tmp += num.substr(0, 3);
+      tmp += '-';
+      tmp += num.substr(3, 3);
+      tmp += '-';
+      tmp += num.substr(6);
+      return tmp;
+    } else {
+      tmp += num.substr(0, 3);
+      tmp += '-';
+      tmp += num.substr(3, 4);
+      tmp += '-';
+      tmp += num.substr(7);
+      return tmp;
+    }
+    return num;
+  };
+
+  const handleCheck = (e: any, id: string) => {
+    console.log(e.target.checked, 'e,target.checked');
+    console.log(id, 'id');
+  };
+
+  return (
+    <>
+      <$.ModalDiv zIndex={zIndex}>
+        <$.CloseBtn onClick={handleClickCloseButton} />
+        <$.ModalContentDiv>
+          <div
+            style={{ display: 'flex', justifyContent: 'center', width: '100%' }}
+          >
+            <$.MainTitle>회원가입</$.MainTitle>
+          </div>
+
+          {modalStep === 0 ? (
+            <>
+              <$.AreaCheckBox>
+                <$.DivForCheck>
+                  <$.RealCheckBox
+                    id="isAllAgree"
+                    defaultChecked={false}
+                    value={isAllAgree}
+                    onClick={(e) => handleCheck(e, 'isAllAgree')}
+                  />
+                  <$.ViewCheckBox />
+                  <$.CheckBoxLabel htmlFor={isAllAgree}>
+                    서비스 약관 전체 동의
+                  </$.CheckBoxLabel>
+                </$.DivForCheck>
+                <$.DivForCheck>자세히</$.DivForCheck>
+              </$.AreaCheckBox>
+
+              <$.AreaCheckBox>
+                <$.DivForCheck>
+                  <$.RealCheckBox
+                    id="agreeOne"
+                    defaultChecked={false}
+                    value={agreeOne}
+                    onClick={(e) => handleCheck(e, 'agreeOne')}
+                  />
+                  <$.ViewCheckBox />
+                  <$.CheckBoxLabel htmlFor={agreeOne}>
+                    개인정보 수집 및 이용 안내 (필수)
+                  </$.CheckBoxLabel>
+                </$.DivForCheck>
+                <$.DivForCheck>자세히</$.DivForCheck>
+              </$.AreaCheckBox>
+
+              <$.AreaCheckBox>
+                <$.DivForCheck>
+                  <$.RealCheckBox
+                    id="agreeTwo"
+                    defaultChecked={false}
+                    value={agreeTwo}
+                    onClick={(e) => handleCheck(e, 'agreeTwo')}
+                  />
+                  <$.ViewCheckBox />
+                  <$.CheckBoxLabel htmlFor={agreeTwo}>
+                    위치정보 수집 및 이용 안내 (필수)
+                  </$.CheckBoxLabel>
+                </$.DivForCheck>
+                <$.DivForCheck>자세히</$.DivForCheck>
+              </$.AreaCheckBox>
+
+              <$.AreaCheckBox>
+                <$.DivForCheck>
+                  <$.RealCheckBox
+                    id="agreeThree"
+                    defaultChecked={false}
+                    value={agreeThree}
+                    onClick={(e) => handleCheck(e, 'agreeThree')}
+                  />
+                  <$.ViewCheckBox />
+                  <$.CheckBoxLabel htmlFor={agreeThree}>
+                    제 3자 제공 동의 (필수)
+                  </$.CheckBoxLabel>
+                </$.DivForCheck>
+                <$.DivForCheck>자세히</$.DivForCheck>
+              </$.AreaCheckBox>
+
+              <$.AreaCheckBox>
+                <$.DivForCheck>
+                  <$.RealCheckBox
+                    id="agreeFour"
+                    defaultChecked={false}
+                    value={agreeFour}
+                    onClick={(e) => handleCheck(e, 'agreeFour')}
+                  />
+                  <$.ViewCheckBox />
+                  <$.CheckBoxLabel htmlFor={agreeFour}>
+                    마케팅 수신 동의
+                  </$.CheckBoxLabel>
+                </$.DivForCheck>
+                <$.DivForCheck>자세히</$.DivForCheck>
+              </$.AreaCheckBox>
+            </>
+          ) : (
+            <>
+              <$.InputLabel>계정 이메일</$.InputLabel>
+              <$.InputBox>
+                <$.Input
+                  value={email}
+                  onChange={handleEmail}
+                  placeholder="이메일 주소 입력"
+                />
+              </$.InputBox>
+              {!isVaildEmail && (
+                <$.ErrorMessage>이미 사용중인 이메일 계정입니다</$.ErrorMessage>
+              )}
+
+              <$.InputLabel>비밀번호</$.InputLabel>
+              <$.InputBox>
+                <$.Input
+                  value={password}
+                  onChange={handlePassword}
+                  placeholder="비밀번호 (8~32자리)"
+                />
+              </$.InputBox>
+              {!isValidPassword && (
+                <$.ErrorMessage>
+                  영문 대소문, 특수문자 포함 8자리 이상이어야 합니다
+                </$.ErrorMessage>
+              )}
+
+              <$.InputBox>
+                <$.Input
+                  value={passwordConfirm}
+                  onChange={handlePasswordConfirm}
+                  placeholder="비밀번호 확인"
+                />
+              </$.InputBox>
+              {!isValidPasswordConfirm && (
+                <$.ErrorMessage>
+                  입력하신 비밀번호가 일치하지 않습니다
+                </$.ErrorMessage>
+              )}
+
+              <$.InputLabel>닉네임</$.InputLabel>
+              <$.InputBox>
+                <$.Input
+                  value={nickName}
+                  onChange={handleNickname}
+                  placeholder="닉네임을 입력해주세요."
+                />
+              </$.InputBox>
+
+              <$.InputLabel>휴대전화</$.InputLabel>
+              <$.InputBox>
+                <$.Input
+                  id="phoneNum"
+                  name="phoneNum"
+                  type="text"
+                  value={phoneNum}
+                  onChange={handlePhoneNum}
+                  placeholder="010-0000-0000"
+                  maxLength="13"
+                />
+              </$.InputBox>
+            </>
+          )}
+
+          <$.SignUpButton>
+            {modalStep === 0 ? '동의하고 회원가입하기' : '회원가입'}
+          </$.SignUpButton>
+        </$.ModalContentDiv>
+      </$.ModalDiv>
+    </>
+  );
+};
+
+export default SignUp;
---
+++ components/User/SignUp/SignUpView.tsx
@@ -0,0 +1,185 @@
+import React from 'react';
+import styled from 'styled-components';
+import painter from '~/styles/theme/painter';
+
+export const MainTitle = styled.span`
+  color: ${painter.grayscale[1]};
+  font-family: Noto Sans CJK KR;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 24px;
+`;
+
+export const InputLabel = styled.label`
+  color: ${painter.grayscale[1]};
+  font-family: Noto Sans CJK KR;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 16px;
+  margin: 30px 0 10px;
+`;
+
+export const InputBox = styled.div`
+  height: 48px;
+  width: 100%;
+  margin-top: 8px;
+  border-bottom: 1px solid ${painter.grayscale[8]};
+  background-color: transparent;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Input = styled.input.attrs({ type: 'text' })`
+  display: block;
+  width: 100%;
+  height: 100%;
+  padding: 0 12px;
+  font-size: 16px;
+  color: ${painter.grayscale[7]};
+  ${painter.form.placeholder('#ADB5BD')}
+`;
+
+export const SignUpButton = styled.button`
+  outline: none;
+  border: none;
+  border-radius: 8px;
+  background-color: ${painter.primary.basic};
+  cursor: pointer;
+  width: 100%;
+  height: 50px;
+  color: ${painter.basic.white};
+  margin: 40px 0;
+`;
+
+export const AreaCheckBox = styled.div`
+  margin: 20px 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const DivForCheck = styled.div`
+  color: ${painter.grayscale[5]};
+`;
+
+export const ViewCheckBox = styled.div`
+  display: inline-block;
+  width: 18px;
+  height: 18px;
+  border-radius: 3px;
+  background-color: transparent;
+  vertical-align: top;
+  cursor: pointer;
+`;
+
+export const RealCheckBox = styled.input.attrs({ type: 'checkbox' })`
+  &:checked {
+    background-color: yellow;
+
+    &::before {
+      content: '';
+      width: 18px;
+      height: 18px;
+      margin: auto;
+      border: solid ${painter.basic.white};
+      border-width: 0 0 2px 2px;
+      transform: rotate(-45deg);
+      background-color: yellow;
+    }
+  }
+`;
+
+export const CheckBoxLabel = styled.label`
+  display: inline-block;
+  position: relative;
+  z-index: 1;
+  margin-left: -18px;
+  padding-left: 22px;
+  vertical-align: top;
+  color: ${painter.grayscale[5]};
+  cursor: pointer;
+  font-family: Noto Sans CJK KR;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 24px;
+`;
+
+export const ModalDiv = styled.div<{ zIndex: string }>`
+  z-index: ${(props) => props.zIndex || '10000'};
+  position: absolute;
+  min-width: 450px;
+  min-height: 600px;
+  left: 40%;
+  top: 10%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
+
+export const ModalContentDiv = styled.div`
+  background-color: ${painter.grayscale[9]};
+  width: 100%;
+  min-height: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  overflow: auto;
+  padding: 45px 25px;
+  border-radius: 16px;
+`;
+
+export const CloseBtn = styled.img.attrs({
+  src: '/modal_close_btn.png',
+})`
+  cursor: pointer;
+  width: 36px;
+  height: 36px;
+`;
+
+export const EmailCheckButton = styled.button`
+  border: none;
+  outline: none;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 24px;
+  width: 129px;
+  height: 40px;
+  background: ${painter.grayscale['1']};
+  border-radius: 8px;
+`;
+
+export const Span = styled.span`
+  font-family: Noto Sans CJK KR;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  color: ${painter.grayscale['4']};
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+  margin: 0px 10px;
+`;
+
+export const ErrorMessage = styled.span`
+  font-family: Noto Sans CJK KR;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 16px;
+  color: #ff5b5b;
+`;
+
+export const CheckboxAdvice = styled.p`
+  margin-top: 8px;
+  font-size: 12px;
+  line-height: 16px;
+  color: ${painter.grayscale[6]};
+`;
